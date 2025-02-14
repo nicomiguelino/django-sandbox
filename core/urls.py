aa00273c70457c952741ab466dfda0d438ff7a83
nicomiguelino <nicomiguelino2014@gmail.com>
2025-02-14 11:11:23 -0800

    feat: create a new page for integrations

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, IntegrationsView
 
 app_name = 'core'
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('integrations/', IntegrationsView.as_view(), name='integrations'),
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = 'core/index.html'
+
+
+class IntegrationsView(TemplateView):
+    template_name = 'core/integrations.html'
