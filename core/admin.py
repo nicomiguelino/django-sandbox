4d22ac834227df34cef29e7260191561f7c867bc
nicomiguelino <nicomiguelino2014@gmail.com>
2025-02-23 13:33:57 -0800

    feat: write admin code for the new `APIKey` model

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import GoogleCredentials
+from .models import APIKey, GoogleCredentials
 
 @admin.register(GoogleCredentials)
 class GoogleCredentialsAdmin(admin.ModelAdmin):
@@ -11,3 +11,11 @@ class GoogleCredentialsAdmin(admin.ModelAdmin):
     def has_add_permission(self, request):
         # Prevent manual creation through admin as credentials should only be created through OAuth
         return False
+
+@admin.register(APIKey)
+class APIKeyAdmin(admin.ModelAdmin):
+    list_display = ('user', 'key', 'name', 'created_at', 'last_used', 'is_active')
+    list_filter = ('created_at', 'last_used', 'is_active')
+    search_fields = ('user__email', 'user__username', 'name')
+    readonly_fields = ('created_at', 'last_used')
+
